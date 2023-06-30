1b415ce5267b339fe64f6bcaf03a3f1e7d2fd76f
Marcus R. Breese <marcus.breese@ucsf.edu>
2023-06-30 07:05:29 -0700

    upped mem limit, prioritized hg38F and 150bp

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-for size in 100 150; do
-
-./mappability.cgp \
-    --outdir hg38 \
-    --name hg38M \
-    --fasta /N/p/spcg/resources/indexes/GRCh38.p5/genomeM.fa \
-    --chrom chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM \
-    --size $size \
-    --threads 4 
+for size in 150 100; do
 
 ./mappability.cgp \
     --outdir hg38 \
@@ -16,6 +8,14 @@ for size in 100 150; do
     --fasta /N/p/spcg/resources/indexes/GRCh38.p5/genomeM.fa \
     --index /N/p/spcg/resources/indexes/GRCh38.p5/genomeF.fa \
     --chrom chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrM \
+    --size $size \
+    --threads 4 
+
+./mappability.cgp \
+    --outdir hg38 \
+    --name hg38M \
+    --fasta /N/p/spcg/resources/indexes/GRCh38.p5/genomeM.fa \
+    --chrom chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM \
     --size $size \
     --threads 4 
 
